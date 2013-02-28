db45c0350498113bfd7e2ec267fb62d700a708ff
Luke Campbell <luke.s.campbell@gmail.com>
2013-02-28 09:34:50 -0500

    Finished preliminary bindings for trans_blob

--- transaction/bindings.pyx
+++ transaction/bindings.pyx
@@ -1,7 +1,43 @@
 
 cdef extern from "trans.h":
     int trans_create_blob( char *infile,  char *outfile, unsigned char *digest) 
+    int TRANS_OK
+
+cdef extern from "openssl/sha.h":
+    int SHA_DIGEST_LENGTH
+
+cimport stdlib
+
+hex_chars = 'abcdef1234567890'
+
+import os
+import random
+def create_blob(bytes infile, bytes outfile):
+    cdef unsigned char *digest
+    cdef char *c_infile = infile
+    cdef char *c_outfile 
+    cdef int status
+    if not os.path.exists(infile):
+        raise IOError('%s does not exist' % infile)
+        
+    if not outfile:
+        dirname = os.path.dirname(infile)
+        tmpname = ''.join([random.choice(hex_chars) for i in xrange(20)])
+        tmpfile = os.path.join(dirname,tmpname)
+        c_outfile = tmpfile
+    else:
+        c_outfile = outfile
+
+    try:
+        digest = <unsigned char *>stdlib.malloc(SHA_DIGEST_LENGTH)
+        status = trans_create_blob(c_infile, c_outfile, digest)
+        if not status == TRANS_OK:
+            raise IOError('Error creating blob')
+        py_string = ''.join('%02x' % digest[i] for i in xrange(SHA_DIGEST_LENGTH))
+        return py_string
+    finally:
+        stdlib.free(digest)
 
 
-def create_blob(str infile, str outfile):
-    pass
+    
+
